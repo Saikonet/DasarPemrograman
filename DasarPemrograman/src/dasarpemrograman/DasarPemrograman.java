308147525549b8d837d99cd59de4b66da9813deb
Saikonet <30762859+Saikonet@users.noreply.github.com>
2018-01-02 10:47:47 +0700

    Add files via upload

---
+++ DasarPemrograman/src/dasarpemrograman/DasarPemrograman.java
@@ -0,0 +1,16 @@
+package dasarpemrograman;
+
+/**
+ *
+ * @author User
+ */
+public class DasarPemrograman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new FormJual().setVisible(true);
+    }
+    
+}
